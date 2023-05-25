8cc13ece225017661191055adf244f224a33c1e2
eVelynOtu <133481129+eVelynOtu@users.noreply.github.com>
2023-05-25 08:10:22 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = "Otuu Evelyn Ugo";
+$regnumber = "2021/HND/33495/CS";
+$soo = "Ebonyi State";
+$topic_of_project = "Design and implementation of birth control mobile application
+Case study of matter hospital afikpo";
+
+// Output the information to the screen.
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic_of_project<br>";
+
+?>
